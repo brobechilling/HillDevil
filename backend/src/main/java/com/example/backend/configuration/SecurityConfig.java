1220da170db13561d5ae3aa1899910d5ed314525
Quoc Nguyen <quocnguyen112505@gmail.com>
2025-10-22 10:21:45 +0700

    test public endpoint

--- backend/src/main/java/com/example/backend/configuration/SecurityConfig.java
+++ backend/src/main/java/com/example/backend/configuration/SecurityConfig.java
@@ -21,7 +21,7 @@ import com.example.backend.entities.RoleName;
 public class SecurityConfig {
 
 
-    private final String[] PUBLIC_ENDPOINTS = {"/api/auth/token", "/api/auth/logout", "/api/auth/refresh", "/api/users/signup", "/api/payments/**", "/api/subscriptions/**"};
+    private final String[] PUBLIC_ENDPOINTS = {"/api/auth/token", "/api/auth/logout", "/api/auth/refresh", "/api/users/signup"};
     private final String[] ADMIN_ENDPOINTS = {"/api/users/**", "/api/roles/**"};
     private final String[] RESTAURANT_OWNER_ENDPOINTS = {};
     private final String[] BRANCH_MANAGER_ENDPOINTS = {};
