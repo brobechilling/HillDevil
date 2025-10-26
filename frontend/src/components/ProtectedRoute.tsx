c201406afb12a42394d1baffffa3a7414419970f
Quoc Nguyen <quocnguyen112505@gmail.com>
2025-10-26 19:35:23 +0700

    update protectedroute later

--- frontend/src/components/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from 'react';
 import { Navigate } from 'react-router-dom';
 import { ROLE_NAME, UserDTO } from '@/dto/user.dto';
 import { StaffAccountDTO } from '@/dto/staff.dto';
+import { useToast } from '@/hooks/use-toast';
 
 interface ProtectedRouteProps {
   children: ReactNode;
@@ -11,6 +12,7 @@ interface ProtectedRouteProps {
 const ProtectedRoute = ({ children, allowedRoles }: ProtectedRouteProps) => {
   const userStr = localStorage.getItem('user');
   const user: UserDTO | StaffAccountDTO | null = userStr ? JSON.parse(userStr) : null;
+  const { toast } = useToast();
 
   if (!user) {
     return <Navigate to="/login" replace />;
@@ -20,8 +22,9 @@ const ProtectedRoute = ({ children, allowedRoles }: ProtectedRouteProps) => {
     const userRole = user.role.name as ROLE_NAME;
     const hasPermission = allowedRoles.includes(userRole);
 
+    // need implmenting sth here
     if (!hasPermission) {
-      return <Navigate to="/dashboard" replace />;
+      return <Navigate to="/" replace />;
     }
   }
 
